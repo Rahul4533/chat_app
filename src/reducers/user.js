05a9b275bd4885422616fb585931817e2addf819
Rahul <Rahulsingh4533@gmail.com>
2024-06-19 10:10:14 +0530

    completed

---
+++ src/reducers/user.js
@@ -0,0 +1,11 @@
+export default function user(state = {}) {
+ 
+   
+    return {
+      id : 1,
+      name : "Sourav Debnath",
+      image : "https://res-console.cloudinary.com/dvl2ojizb/thumbnails/v1/image/upload/v1709035005/anNjN2prazVmeWU2dXlmeWwyeXk=/preview"
+
+    }
+  
+}
